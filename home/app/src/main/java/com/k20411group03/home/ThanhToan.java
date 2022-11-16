f4c967a068ee0c80721e7fe36ecea55a055a663a
Từ Hải <92880549+Resplly2001@users.noreply.github.com>
2022-11-16 11:34:06 +0700

    Thanh toan

--- home/app/src/main/java/com/k20411group03/home/ThanhToan.java
+++ home/app/src/main/java/com/k20411group03/home/ThanhToan.java
@@ -3,12 +3,46 @@ package com.k20411group03.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.k20411group03.adapters.PaymentAdapter;
+import com.k20411group03.adapters.ProductAdapter;
+import com.k20411group03.home.databinding.ActivityMainBinding;
+import com.k20411group03.home.databinding.ActivityThanhToanBinding;
+import com.k20411group03.models.Payment;
+import com.k20411group03.models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ThanhToan extends AppCompatActivity {
+
+    ActivityThanhToanBinding binding;
+    PaymentAdapter adapter;
+    List<Payment> payments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_thanh_toan);
+        //setContentView(R.layout.activity_main);
+
+        binding = ActivityThanhToanBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        loadData();
+    }
+
+    private void loadData() {
+        payments = new ArrayList<>();
+
+        payments.add(new Payment(false, R.drawable.payment_cash, "Thanh toán tiền mặt", "Thanh toán khi nhận hàng"));
+        payments.add(new Payment(false, R.drawable.payment_momo, "Ví Momo", ""));
+
+        payments.add(new Payment(false, R.drawable.payment_zalo, "Ví ZaloPay", ""));
+
+        payments.add(new Payment(false, R.drawable.payment_banking, "Thẻ ATM/ Internet Banking", "Hỗ trợ hơn 20 ngân hàng"));
+
+        adapter = new PaymentAdapter(ThanhToan.this, R.layout.payment_method, payments);
+        binding.lvPhuongThucThanhToan.setAdapter(adapter);
     }
 }

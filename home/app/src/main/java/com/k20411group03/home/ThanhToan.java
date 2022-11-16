c6ea816c566912734faa7b909887fa479e7714f6
Từ Hải <92880549+Resplly2001@users.noreply.github.com>
2022-11-16 09:31:50 +0700

    thanh toan

---
+++ home/app/src/main/java/com/k20411group03/home/ThanhToan.java
@@ -0,0 +1,14 @@
+package com.k20411group03.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThanhToan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanh_toan);
+    }
+}

0221783667e55685fdc638da720d3112bcd2a0cd
unknown <haodh20411@st.uel.edu.vn>
2022-11-22 14:29:24 +0700

    Demo giao dien trang chu

---
+++ home/app/src/main/java/com/k20411group03/TrangChu.java
@@ -0,0 +1,16 @@
+package com.k20411group03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.k20411group03.home.R;
+
+public class TrangChu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.k20411group03.home.R.layout.activity_trang_chu);
+    }
+}

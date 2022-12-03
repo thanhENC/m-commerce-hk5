c77b0c3596dc5751aa684c2854f70c882762be98
Pnhienphuong <79975692+Pnhienphuong@users.noreply.github.com>
2022-12-03 15:17:46 +0700

    add dialog for customer

--- home/app/src/main/java/com/k20411group03/home/member1.java
+++ home/app/src/main/java/com/k20411group03/home/member1.java
@@ -1,9 +1,14 @@
 package com.k20411group03.home;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.k20411group03.home.databinding.ActivityMember1Binding;
@@ -17,14 +22,31 @@ public class member1 extends AppCompatActivity {
         //setContentView(R.layout.activity_member1);
         binding= ActivityMember1Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         binding.btnDangxuat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Open activity 2
-                Intent intent = new Intent(member1.this, TrangChu.class);
-                startActivity(intent);
+                final Dialog dialog = new Dialog(member1.this);
+                dialog.setContentView(R.layout.dialog_dangxuat);
+                dialog.setCanceledOnTouchOutside(false);
+                TextView txtDangXuat = dialog.findViewById(R.id.txtYes);
+                txtDangXuat.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(member1.this, TrangChu.class);
+                        startActivity(intent);
+                    }
+                });
+                TextView txtHuy = dialog.findViewById(R.id.txtQuit);
+                txtHuy.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
             }
         });
     }
+
+
 }

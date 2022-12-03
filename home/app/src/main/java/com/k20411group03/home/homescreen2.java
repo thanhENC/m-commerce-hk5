b52c3a63d94909ed3904a7ad19c8997d9ad4eb35
duckndonut <107560219+duckndonut@users.noreply.github.com>
2022-12-03 17:31:13 +0700

    Welcome + Cookies

--- home/app/src/main/java/com/k20411group03/home/homescreen2.java
+++ home/app/src/main/java/com/k20411group03/home/homescreen2.java
@@ -1,14 +1,36 @@
 package com.k20411group03.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.k20411group03.home.databinding.ActivityHomescreen2Binding;
+import com.k20411group03.home.databinding.ActivityHomescreenBinding;
+
 public class homescreen2 extends AppCompatActivity {
+    ActivityHomescreen2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen2);
+        Log.i("HomeScreen2","onCreate");
+        binding = ActivityHomescreen2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        addEvents();
+    }
+
+    private void addEvents() {
+        binding.btnDongY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(homescreen2.this,TrangChu.class);
+                startActivity(intent);
+            }
+        });
     }
 }

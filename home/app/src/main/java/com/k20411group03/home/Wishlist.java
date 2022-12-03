b78673db2cc516711f3a5e25086f5ebf3f149c8d
unknown <haodh20411@st.uel.edu.vn>
2022-12-03 13:59:39 +0700

    Bottom navigation bar

--- home/app/src/main/java/com/k20411group03/home/Wishlist.java
+++ home/app/src/main/java/com/k20411group03/home/Wishlist.java
@@ -1,54 +1,54 @@
 package com.k20411group03.home;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class scan1 extends AppCompatActivity {
+public class Wishlist extends AppCompatActivity {
     BottomNavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan1);
+        setContentView(R.layout.activity_wishlist);
 
         addEvents();
     }
     private void addEvents(){
         //Bottom navigation
-        navigationView = findViewById(R.id.mn_scan);
-        navigationView.setSelectedItemId(R.id.item_scan);
+        navigationView = findViewById(R.id.mn_wishlist);
+        navigationView.setSelectedItemId(R.id.item_wishlist);
         navigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
-                    case R.id.item_scan:
+                    case  R.id.item_wishlist:
                         return true;
-                    case  R.id.item_member1:
-                        Intent intent1 = new Intent(getApplicationContext(),member1.class);
+                    case R.id.item_scan:
+                        Intent intent1 = new Intent(getApplicationContext(),scan1.class);
                         intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent1);
                         overridePendingTransition(0,0);
                         return true;
-                    case  R.id.item_noti:
-                        Intent intent2 =new Intent(getApplicationContext(),ThongBao.class);
+                    case  R.id.item_member1:
+                        Intent intent2 = new Intent(getApplicationContext(),member1.class);
                         intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent2);
                         overridePendingTransition(0,0);
                         return true;
-                    case  R.id.item_home:
-                        Intent intent3 =new Intent(getApplicationContext(),TrangChu.class);
+                    case  R.id.item_noti:
+                        Intent intent3 =new Intent(getApplicationContext(),ThongBao.class);
                         intent3.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent3);
                         overridePendingTransition(0,0);
                         return true;
-                    case  R.id.item_wishlist:
-                        Intent intent4 =new Intent(getApplicationContext(),Wishlist.class);
+                    case  R.id.item_home:
+                        Intent intent4 =new Intent(getApplicationContext(),TrangChu.class);
                         intent4.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent4);
                         overridePendingTransition(0,0);
@@ -57,6 +57,5 @@ public class scan1 extends AppCompatActivity {
                 return false;
             }
         });
-
     }
 }
--- home/app/src/main/java/com/k20411group03/home/member1.java
+++ home/app/src/main/java/com/k20411group03/home/member1.java
@@ -1,14 +1,61 @@
 package com.k20411group03.home;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class member1 extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+public class member1 extends AppCompatActivity {
+    BottomNavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member1);
+        addEvents();
+    }
+    private void addEvents(){
+        //Bottom navigation
+        navigationView = findViewById(R.id.mn_membership);
+        navigationView.setSelectedItemId(R.id.item_member1);
+        navigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case  R.id.item_member1:
+                        return true;
+                    case R.id.item_scan:
+                        Intent intent = new Intent(getApplicationContext(),scan1.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                        overridePendingTransition(0,0);
+                        return true;
+                    case  R.id.item_wishlist:
+                        Intent intent1 = new Intent(getApplicationContext(),Wishlist.class);
+                        intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent1);
+                        overridePendingTransition(0,0);
+                        return true;
+                    case  R.id.item_noti:
+                        Intent intent2 =new Intent(getApplicationContext(),ThongBao.class);
+                        intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent2);
+                        overridePendingTransition(0,0);
+                        return true;
+                    case  R.id.item_home:
+                        Intent intent3 =new Intent(getApplicationContext(),TrangChu.class);
+                        intent3.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent3);
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }

fe824760cd6d97076ffce531cc0a82d66936a459
Pnhienphuong <79975692+Pnhienphuong@users.noreply.github.com>
2022-12-03 13:28:23 +0700

    add log out in member screen

--- home/app/src/main/java/com/k20411group03/home/member1.java
+++ home/app/src/main/java/com/k20411group03/home/member1.java
@@ -1,14 +1,30 @@
 package com.k20411group03.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class member1 extends AppCompatActivity {
+import com.k20411group03.home.databinding.ActivityMember1Binding;
+import com.k20411group03.home.databinding.ActivityVoucherMainBinding;
 
+public class member1 extends AppCompatActivity {
+    ActivityMember1Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_member1);
+        //setContentView(R.layout.activity_member1);
+        binding= ActivityMember1Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnDangxuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open activity 2
+                Intent intent = new Intent(member1.this, TrangChu.class);
+                startActivity(intent);
+            }
+        });
     }
 }

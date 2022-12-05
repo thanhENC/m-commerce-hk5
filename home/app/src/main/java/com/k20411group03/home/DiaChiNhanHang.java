a6014df1122a1b93620a8b53758b1e130b255aa4
Đinh Hoàn Hảo <113403410+paferuto@users.noreply.github.com>
2022-12-05 23:40:55 +0700

    Update DiaChiNhanHang.java

--- home/app/src/main/java/com/k20411group03/home/DiaChiNhanHang.java
+++ home/app/src/main/java/com/k20411group03/home/DiaChiNhanHang.java
@@ -253,34 +253,34 @@ public class DiaChiNhanHang extends Activity {
     }
 
 
-    //Thêm action
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+//     //Thêm action
+//     @Override
+//     public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+//         getMenuInflater().inflate(R.menu.main, menu);
+//         return super.onCreateOptionsMenu(menu);
+//     }
 
-    //Sự kiện action bar
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_search:
-                Intent intentSearch = new Intent(DiaChiNhanHang.this, ActivitySearch.class);
-                startActivity(intentSearch);
-                break;
-            case R.id.action_cart:
-                Intent intentCart = new Intent(DiaChiNhanHang.this, MainActivity.class);
-                startActivity(intentCart);
-                break;
-            case R.id.action_menu:
-                Intent intentMenu = new Intent(DiaChiNhanHang.this, MainMenu.class);
-                startActivity(intentMenu);
-                break;
-        }
+//     //Sự kiện action bar
+//     @Override
+//     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//         switch (item.getItemId()) {
+//             case R.id.action_search:
+//                 Intent intentSearch = new Intent(DiaChiNhanHang.this, ActivitySearch.class);
+//                 startActivity(intentSearch);
+//                 break;
+//             case R.id.action_cart:
+//                 Intent intentCart = new Intent(DiaChiNhanHang.this, MainActivity.class);
+//                 startActivity(intentCart);
+//                 break;
+//             case R.id.action_menu:
+//                 Intent intentMenu = new Intent(DiaChiNhanHang.this, MainMenu.class);
+//                 startActivity(intentMenu);
+//                 break;
+//         }
 
-        return super.onOptionsItemSelected(item);
-    }
+//         return super.onOptionsItemSelected(item);
+//     }
 
     private void addEvents() {
 

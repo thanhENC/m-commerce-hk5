a7cb991e80c78fd2f11715ee6a9d660e2f04ef7f
Thanh <thanhenc@gmail.com>
2022-12-06 08:29:36 +0700

    Fix

--- home/app/src/main/java/com/k20411group03/home/ThanhToan.java
+++ home/app/src/main/java/com/k20411group03/home/ThanhToan.java
@@ -1,57 +1,42 @@
 package com.k20411group03.home;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.ViewPager2;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.TypefaceSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Switch;
-import androidx.appcompat.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
-import com.k20411group03.adapters.BannerAdapter;
-import com.k20411group03.adapters.CategoryAdapter;
-import com.k20411group03.adapters.ItemRecyclerAdapter;
-import com.k20411group03.models.Banners;
-import com.k20411group03.models.Item;
-import com.k20411group03.models.category;
+import com.k20411group03.DatabaseHelper;
+import com.k20411group03.adapters.PaymentAdapter;
+import com.k20411group03.home.databinding.ActivityThanhToanBinding;
+import com.k20411group03.models.Payment;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class TrangChu extends AppCompatActivity {
-    ViewPager2 viewPager;
-    ArrayList<Banners> bannerList;
-    RecyclerView rcvFlashsale, rcvNewArrival, rcvForYou;
-    GridView gvCategory;
-    CategoryAdapter categoryAdapter;
-    BottomNavigationView navigationView;
-    Timer timer;
+public class ThanhToan extends AppCompatActivity {
 
+    ActivityThanhToanBinding binding;
+    PaymentAdapter adapter;
+    List<Payment> payments;
+    DatabaseHelper db;
+
+    //ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trang_chu);
+        //setContentView(R.layout.activity_main);
 
         //Custom action bar
         ActionBar actionBar = getSupportActionBar();
@@ -61,50 +46,35 @@ public class TrangChu extends AppCompatActivity {
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
 
-        //Set up Flash sale recyclerview
-        rcvFlashsale = findViewById(R.id.rcv_Flashsale);
-        ItemRecyclerAdapter saleItemAdapter = new ItemRecyclerAdapter(this);
-        saleItemAdapter.setData(getListSaleItem());
-        LinearLayoutManager saleLayoutManager = new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
-        rcvFlashsale.setLayoutManager(saleLayoutManager);
-        rcvFlashsale.setAdapter(saleItemAdapter);
+        binding = ActivityThanhToanBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        //Gridview category
-        gvCategory = findViewById(R.id.gv_Category);
-        categoryAdapter = new CategoryAdapter(TrangChu.this,R.layout.category_layout,getCategory());
-        gvCategory.setAdapter(categoryAdapter);
-
-        //Set up New Arrival recyclerview
-        rcvNewArrival = findViewById(R.id.rcv_NewArrival);
-        ItemRecyclerAdapter newItemAdapter = new ItemRecyclerAdapter(this);
-        newItemAdapter.setData(getListSaleItem());
-        LinearLayoutManager newLayoutManager = new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
-        rcvNewArrival.setLayoutManager(newLayoutManager);
-        rcvNewArrival.setAdapter(newItemAdapter);
-
-        //Set up For you recyclerview
-        rcvForYou = findViewById(R.id.rcv_ForYou);
-        ItemRecyclerAdapter foryouItemAdapter = new ItemRecyclerAdapter(this);
-        foryouItemAdapter.setData(getListSaleItem());
-        LinearLayoutManager forYouLayoutManager = new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
-        rcvForYou.setLayoutManager(forYouLayoutManager);
-        rcvForYou.setAdapter(foryouItemAdapter);
-
-        //Set up viewpager
-        bannerList = new ArrayList<>();
-        viewPager = findViewById(R.id.vp_HomeBanner);
-        int[] images = {R.drawable.banner2,R.drawable.banner1,R.drawable.banner3};
-        for (int i =0; i< images.length ; i++){
-
-            Banners banner = new Banners(images[i]);
-            bannerList.add(banner);
-        }
-        BannerAdapter bannerAdapter =(BannerAdapter) new BannerAdapter(bannerList);
-        viewPager.setAdapter(bannerAdapter);
-
-        autoSlide();
+        createDb();
+        loadData();
         addEvents();
+    }
 
+    private void createDb() {
+        db = new DatabaseHelper(ThanhToan.this);
+        db.createSampleData();
+    }
+
+    private void loadData() {
+        payments = new ArrayList<>();
+        Cursor c = db.getData("SELECT * FROM " + DatabaseHelper.TBL_NAME);
+        while (c.moveToNext()){
+            payments.add(new Payment(c.getInt(0),
+                    c.getString(1),
+                    c.getString(2),
+                    c.getInt(3)
+            ));
+        }
+        c.close();
+
+        //Init data
+        //binding.lvPhuongThucThanhToan.setChoiceMode(binding.lvPhuongThucThanhToan.CHOICE_MODE_SINGLE);
+        adapter = new PaymentAdapter(ThanhToan.this, R.layout.payment_method, payments);
+        binding.lvPhuongThucThanhToan.setAdapter(adapter);
     }
 
     //Thêm action
@@ -120,29 +90,29 @@ public class TrangChu extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_search:
-                Intent intentSearch = new Intent(TrangChu.this, ActivitySearch.class);
+                Intent intentSearch = new Intent(this, ActivitySearch.class);
                 startActivity(intentSearch);
                 break;
             case R.id.action_cart:
-                Intent intentCart = new Intent(TrangChu.this, MainActivity.class);
+                Intent intentCart = new Intent(this, MainActivity.class);
                 startActivity(intentCart);
                 break;
             case R.id.action_BoSuuTap:
-                Intent intentBoSuuTap = new Intent(TrangChu.this, ProductCollection.class);
+                Intent intentBoSuuTap = new Intent(this, ProductCollection.class);
                 intentBoSuuTap.putExtra("screenTitle", "Bộ sưu tập mới");
                 startActivity(intentBoSuuTap);
                 break;
             case R.id.action_HangMoiVe:
-                Intent intentSanPhamMoi = new Intent(TrangChu.this, ProductCollection.class);
+                Intent intentSanPhamMoi = new Intent(this, ProductCollection.class);
                 intentSanPhamMoi.putExtra("screenTitle", "Hàng mới về");
                 startActivity(intentSanPhamMoi);
                 break;
             case R.id.action_Flashsale:
-                Intent intentFlashsale = new Intent(TrangChu.this, FlashSaleScreen.class);
+                Intent intentFlashsale = new Intent(this, FlashSaleScreen.class);
                 startActivity(intentFlashsale);
                 break;
             case R.id.action_SanPham:
-                Intent intentSanPham = new Intent(TrangChu.this, ProductCollection.class);
+                Intent intentSanPham = new Intent(this, ProductCollection.class);
                 intentSanPham.putExtra("screenTitle", "Sản phẩm");
                 startActivity(intentSanPham);
                 break;
@@ -151,132 +121,37 @@ public class TrangChu extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //"Ai nạp data thì sửa cái này"
-    private List<Item> getListSaleItem(){
-        List<Item> list = new ArrayList<>();
-        list.add(new Item(1,1,"Áo sơ mi ca rô xanh rêu nhạt",150,R.drawable.somi,"Áo sơ mi caro",200000,300000,33,4.5,29,new String[]{"Red","Blue"},new String[]{"M","L","XL"}));
-        list.add(new Item(1,1,"Áo sơ mi ca rô xanh rêu nhạt",150,R.drawable.somi,"Áo sơ mi caro",200000,300000,33,4.5,29,new String[]{"Red","Blue"},new String[]{"M","L","XL"}));
-        list.add(new Item(1,1,"Áo sơ mi ca rô xanh rêu nhạt",150,R.drawable.somi,"Áo sơ mi caro",200000,300000,33,4.5,29,new String[]{"Red","Blue"},new String[]{"M","L","XL"}));
-        list.add(new Item(1,1,"Áo sơ mi ca rô xanh rêu nhạt",150,R.drawable.somi,"Áo sơ mi caro",200000,300000,33,4.5,29,new String[]{"Red","Blue"},new String[]{"M","L","XL"}));
-        list.add(new Item(1,1,"Áo sơ mi ca rô xanh rêu nhạt",150,R.drawable.somi,"Áo sơ mi caro",200000,300000,33,4.5,29,new String[]{"Red","Blue"},new String[]{"M","L","XL"}));
-        list.add(new Item(1,1,"Áo sơ mi ca rô xanh rêu nhạt",150,R.drawable.somi,"Áo sơ mi caro",200000,300000,33,4.5,29,new String[]{"Red","Blue"},new String[]{"M","L","XL"}));        list.add(new Item(1,1,"Áo sơ mi ca rô xanh rêu nhạt",150,R.drawable.somi,"Áo sơ mi caro",200000,300000,33,4.5,29,new String[]{"Red","Blue"},new String[]{"M","L","XL"}));
-        list.add(new Item(1,1,"Áo sơ mi ca rô xanh rêu nhạt",150,R.drawable.somi,"Áo sơ mi caro",200000,300000,33,4.5,29,new String[]{"Red","Blue"},new String[]{"M","L","XL"}));
-        list.add(new Item(1,1,"Áo sơ mi ca rô xanh rêu nhạt",150,R.drawable.somi,"Áo sơ mi caro",200000,300000,33,4.5,29,new String[]{"Red","Blue"},new String[]{"M","L","XL"}));
+    private void addEvents() {
+//        binding.btnConfirmpayment.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //Open gio hang
+//                Intent intent = new Intent(ThanhToan.this, MainActivity.class);
+//                intent.putExtra("Payment", "Momo");
+//
+//                startActivity(intent);
+//                //Để dữ liệu được gửi đi và trả về
+//                //Cach 1: Sử dụng startActivityForResult
+//                //                startActivityForResult(intent,REQUEST_CODE);
+//
+//                //Cach 2: Sử dụng ActivityResultLauncher
+//                launcher.launch(intent);
+//            }
+//        });
 
-
-
-        return list;
-    }
-
-    private List<category> getCategory (){
-        List<category> cateList =new ArrayList<>();
-        cateList.add(new category("AT",R.drawable.cate_tshirt,"Áo thun"));
-        cateList.add(new category("PL",R.drawable.cate_polo,"Áo polo"));
-        cateList.add(new category("SM",R.drawable.cate_shirt,"Áo sơ mi"));
-        cateList.add(new category("AK",R.drawable.cate_jacket,"Áo khoác"));
-        cateList.add(new category("JE",R.drawable.cate_jean,"Quần jean"));
-        cateList.add(new category("TA",R.drawable.cateeasypant,"Quần âu"));
-        cateList.add(new category("KK",R.drawable.cate_kaki,"Quần kaki"));
-        cateList.add(new category("SH",R.drawable.shortpant,"Quần short"));
-        return cateList;
-    }
-
-    private void autoSlide(){
-        if(bannerList == null || bannerList.isEmpty() || viewPager == null){
-            return;
-        }
-        if(timer == null){
-            timer = new Timer();
-        }
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                new Handler(Looper.getMainLooper()).post(new Runnable() {
-                    @Override
-                    public void run() {
-                        int currentItem = viewPager.getCurrentItem();
-                        int totalItem = bannerList.size() - 1;
-                        if(currentItem < totalItem){
-                            currentItem++;
-                            viewPager.setCurrentItem(currentItem);
-                        }else{viewPager.setCurrentItem(0);}
-                    }
-                });
-
-            }
-        }, 1000, 3000);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if(timer != null){
-            timer.cancel();
-            timer =null;
-        }
-    }
-
-    private void addEvents(){
-
-        //Sự kiện khi click vào navigation bar
-        navigationView = findViewById(R.id.mn_home);
-        navigationView.setSelectedItemId(R.id.item_home);
-        navigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.item_home:
-                        return true;
-                    case  R.id.item_member1:
-                        Intent intent1 = new Intent(getApplicationContext(),member1.class);
-                        intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent1);
-                        overridePendingTransition(0,0);
-                        return true;
-                    case  R.id.item_noti:
-                        Intent intent2 =new Intent(getApplicationContext(),ThongBao.class);
-                        intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent2);
-                        overridePendingTransition(0,0);
-                        return true;
-                    case  R.id.item_scan:
-                        Intent intent3 =new Intent(getApplicationContext(),scan1.class);
-                        intent3.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent3);
-                        overridePendingTransition(0,0);
-                        return true;
-                    case  R.id.item_wishlist:
-                        Intent intent4 =new Intent(getApplicationContext(),Wishlist.class);
-                        intent4.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent4);
-                        overridePendingTransition(0,0);
-                        return true;
-                }
-                return false;
-            }
-        });
-
-
-        //Sự kiện khi click vào các item trong danh mục
-        gvCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // When clicking on the item, the dialog will appear
+        binding.lvPhuongThucThanhToan.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                category c = (category) gvCategory.getItemAtPosition(i);
-                String cateID = c.getCateID();
-                Intent intent = new Intent(TrangChu.this,DanhMuc.class);
-                intent.putExtra("Cate", cateID);
-                startActivity(intent);
-            }
-        });
+                //Get product info
+                Payment p = (Payment) adapter.getItem(i);
+                Intent intent1 = new Intent(ThanhToan.this, ThanhToanPre.class);
+                intent1.putExtra("Thanhtoan", p.getPaymentName());
+                intent1.putExtra("Image", p.getPaymentImage());
+                startActivity(intent1);
+                //ActivityResultLauncher<Intent> launcher = null;
+                //launcher.launch(intent1);
 
-
-        //click vào viewpager chuyển sang trang bộ sưu tập (chưa xong)
-        viewPager.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(TrangChu.this,ProductCollection.class);
-                startActivity(intent);
             }
         });
     }

15844e331b8201f9823e54674b4b56463354f90a
Pnhienphuong <79975692+Pnhienphuong@users.noreply.github.com>
2022-12-06 12:32:42 +0700

    dangnhap- dangky chinh sua- done

--- home/app/src/main/java/com/k20411group03/Utilities/Constants.java
+++ home/app/src/main/java/com/k20411group03/Utilities/Constants.java
@@ -1,11 +1,17 @@
 package com.k20411group03.Utilities;
 
 public class Constants {
-    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_COLLECTION_USERS = "CustomerInfo";
     public static final String KEY_NAME = "name";
     public static final String KEY_EMAIL = "email";
     public static final String KEY_PASSWORD = "password";
     public static final String KEY_PHONE = "phone";
+    public static final String KEY_TINH = "Thành Phố/ Tỉnh";
+    public static final String KEY_QUAN = "Quận/ Huyện";
+    public static final String KEY_PHUONG = "Phường/ xã";
+    public static final String KEY_HO = "Họ";
+    public static final String KEY_TEN = "Tên";
+    public static final String KEY_DIACHI = "Địa chỉ";
     public static final String KEY_PREFERENCE_NAME = "chatAppPreference";
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
     public  static final String KEY_USER_ID = "userId";
--- home/app/src/main/java/com/k20411group03/home/member1.java
+++ home/app/src/main/java/com/k20411group03/home/member1.java
@@ -1,15 +1,13 @@
 package com.k20411group03.home;
 
-import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
@@ -17,60 +15,109 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.k20411group03.Utilities.Constants;
+import com.k20411group03.Utilities.PreferenceManager;
 import com.k20411group03.home.databinding.ActivityMember1Binding;
+
+import java.util.HashMap;
 
 public class member1 extends AppCompatActivity {
     ActivityMember1Binding binding;
     BottomNavigationView navigationView;
+    private PreferenceManager preferenceManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_member1);
-
-//         //Custom action bar
-//         ActionBar actionBar = getSupportActionBar();
-//         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-//         actionBar.setDisplayShowCustomEnabled(true);
-//         actionBar.setCustomView(R.layout.custom_action_bar);
-//         actionBar.setDisplayUseLogoEnabled(true);
-//         actionBar.setDisplayShowHomeEnabled(true);
-
         binding= ActivityMember1Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+        getToken();
+        setListeners();
         addEvents();
     }
+    private void loadUserDetails(){
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
 
-//     //Thêm action
-//     @Override
-//     public boolean onCreateOptionsMenu(Menu menu) {
+        //mo nay ra neu co anh
+//        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+//        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+//        binding.imageProfile.setImageBitmap(bitmap);
+    }
 
-//         getMenuInflater().inflate(R.menu.main, menu);
-//         return super.onCreateOptionsMenu(menu);
-//     }
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
 
-//     //Sự kiện action bar
-//     @Override
-//     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//         switch (item.getItemId()) {
-//             case R.id.action_search:
-//                 Intent intentSearch = new Intent(member1.this, ActivitySearch.class);
-//                 startActivity(intentSearch);
-//                 break;
-//             case R.id.action_cart:
-//                 Intent intentCart = new Intent(member1.this, MainActivity.class);
-//                 startActivity(intentCart);
-//                 break;
-//             case R.id.action_menu:
-//                 Intent intentMenu = new Intent(member1.this, MainMenu.class);
-//                 startActivity(intentMenu);
-//                 break;
-//         }
+    }
+    //Button Đăng xuất
+    private void setListeners(){
+        binding.btnDangxuat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(member1.this);
+                dialog.setContentView(R.layout.dialog_dangxuat);
+                dialog.setCanceledOnTouchOutside(false);
+                TextView txtDangXuat = dialog.findViewById(R.id.txtYes);
+                txtDangXuat.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        signOut();
+                    }
+                });
+                TextView txtHuy = dialog.findViewById(R.id.txtQuit);
+                txtHuy.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
 
-//         return super.onOptionsItemSelected(item);
-//     }
+            }
 
+        });
+    }
+
+    private void getToken(){
+        // not null token
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful() && task.getResult() != null){
+                        updateToken(task.getResult());
+                    }
+                });
+
+    }
+
+    private void updateToken(String token){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
+                .addOnFailureListener(e -> showToast("Unable to update token"));
+    }
+
+    private void signOut(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), MacDinhMember.class));
+                    // tìm cách nó quay lại dữ liệu cũ yêu cầu đăng nhập lại, đang bị vướng khúc này
+                    finish();
+                })
+                .addOnFailureListener(e -> showToast("Unable to sign out"));
+    }
     private void addEvents(){
         //Bottom navigation
         //navigationView = findViewById(R.id.mn_membership);
@@ -112,30 +159,5 @@ public class member1 extends AppCompatActivity {
             }
         });
 
-        //Button Đăng xuất
-        binding.btnDangxuat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final Dialog dialog = new Dialog(member1.this);
-                dialog.setContentView(R.layout.dialog_dangxuat);
-                dialog.setCanceledOnTouchOutside(false);
-                TextView txtDangXuat = dialog.findViewById(R.id.txtYes);
-                txtDangXuat.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent(member1.this, TrangChu.class);
-                        startActivity(intent);
-                    }
-                });
-                TextView txtHuy = dialog.findViewById(R.id.txtQuit);
-                txtHuy.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        dialog.dismiss();
-                    }
-                });
-                dialog.show();
-            }
-        });
     }
 }
